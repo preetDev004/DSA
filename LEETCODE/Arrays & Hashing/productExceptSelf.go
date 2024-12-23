14974fbe00f148e66e255705231b3c37c7feb95d
preet patel <preetpatelofficial004@gmail.com>
2024-12-23 11:55:03 -0500

    Practice

--- LEETCODE/Arrays & Hashing/productExceptSelf.go
+++ LEETCODE/Arrays & Hashing/productExceptSelf.go
@@ -1,6 +1,23 @@
 package leetcode
 
-func productExceptSelf(nums []int) []int{
-	var answer []int
-	return answer
+func productExceptSelf(nums []int) []int {
+    length := len(nums)
+    result := make([]int, length)
+	// Initialize result with all elements as 1
+    for i := range result{
+        result[i] = 1
+    }
+	// Left to Right update result
+    prod := 1
+    for i := 0; i < length; i++{
+        result[i] *= prod
+        prod *= nums[i]
+    }
+	// Right to Left update result
+    prod = 1 
+    for i := length - 1; i >= 0; i--{
+        result[i] *= prod
+        prod *= nums[i]
+    }
+    return result
 }
--- LEETCODE/Arrays & Hashing/twoSum.go
+++ LEETCODE/Arrays & Hashing/twoSum.go
@@ -1,3 +1,16 @@
 // Problem Link: https://leetcode.com/problems/two-sum
 
 package leetcode
+
+func twoSum(nums []int, target int) []int {
+	numMap := make(map[int]int)
+
+	for i, num := range nums{
+		ele := target - num
+		if j, ok := numMap[ele]; ok {
+			return []int{j, i}
+		}
+		numMap[num] = i
+	}
+	return []int{}
+}
