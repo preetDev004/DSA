d778d6a56a344395781e52c908b8d7b9be81dddb
Preet Patel <preetpatelofficial004@gmail.com>
2025-08-08 20:55:27 -0400

    learn: creating queue using stack concepts

---
+++ Queues/40_stack_with_queue.py
@@ -0,0 +1,22 @@
+from collections import deque
+
+class MyStack:
+
+    def __init__(self):
+        self.queue = deque()
+
+    def push(self, x: int) -> None:
+        self.queue.append(x)
+
+    def pop(self) -> int:
+        for _ in range(len(self.queue)-1):
+            self.queue.append(self.popleft())
+        return self.queue.popleft()
+
+    def top(self) -> int:
+        return self.queue[0]
+
+    def empty(self) -> bool:
+        return len(self.queue) == 0
+        
+
